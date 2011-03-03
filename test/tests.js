066c5d2794e76bce6eb1e389a3490e55e5d73d46
Chris Johnson <tenorviol@yahoo.com>
2011-03-03 14:48:54 -0800

    cookies can be surrounded by spaces

--- test/tests.js
+++ test/tests.js
@@ -204,7 +204,7 @@ exports.testGetSessionUnescaped = function(test) {
 		url: '/',
 		headers: {
 			// cookie copied from Facebook's Javascript SDK running on Safari
-			cookie: 'fbs_117743971608120="access_token=117743971608120%7C2.vdCKd4ZIEJlHwwtrkilgKQ__.86400.1281049200-1677846385%7CNF_2DDNxFBznj2CuwiwabHhTAHc.&expires=1281049200&secret=u0QiRGAwaPCyQ7JE_hiz1w__&session_key=2.vdCKd4ZIEJlHwwtrkilgKQ__.86400.1281049200-1677846385&sig=7a9b063de0bef334637832166948dcad&uid=1677846385"'
+			cookie: 'junk=foo; fbs_117743971608120="access_token=117743971608120%7C2.vdCKd4ZIEJlHwwtrkilgKQ__.86400.1281049200-1677846385%7CNF_2DDNxFBznj2CuwiwabHhTAHc.&expires=1281049200&secret=u0QiRGAwaPCyQ7JE_hiz1w__&session_key=2.vdCKd4ZIEJlHwwtrkilgKQ__.86400.1281049200-1677846385&sig=7a9b063de0bef334637832166948dcad&uid=1677846385"'
 		}
 	};
 	var facebook = new fbsdk.Facebook({
