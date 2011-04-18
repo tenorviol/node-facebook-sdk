f026044122264c7897484d07c83fba5b1997e0ab
Chris Johnson <tenorviol@yahoo.com>
2011-04-18 14:02:18 -0700

    tabs to spaces; added catch for http exception

--- example/connect.js
+++ example/connect.js
@@ -1,25 +1,29 @@
 var connect = require('connect'),
-	fbsdk = require('facebook-sdk');
+    fbsdk = require('facebook-sdk');
+
+var port = 3000;
 
 connect()
-	.use(connect.favicon())
-	.use(fbsdk.facebook({
-		appId  : 'YOUR APP ID',
-		secret : 'YOUR API SECRET'
-	}))
-	.use(function(req, res, next) {
-		
-		if (req.facebook.getSession()) {
-			res.end('<a href="' + req.facebook.getLogoutUrl() + '">Logout</a>');
-			
-			// get my graph api information
-			req.facebook.api('/me', function(me) {
-			    console.log(me);
-			});
-			
-		} else {
-		    res.end('<a href="' + req.facebook.getLoginUrl() + '">Login</a>');
-		}
-		
-	})
-	.listen(3000);
+  .use(connect.favicon())
+  .use(fbsdk.facebook({
+    appId  : 'YOUR APP ID',
+    secret : 'YOUR APP SECRET'
+  }))
+  .use(function(req, res, next) {
+    
+    if (req.facebook.getSession()) {
+      
+      // get my graph api information
+      req.facebook.api('/me', function(me) {
+        console.log(me);
+      });
+      
+      res.end('<a href="' + req.facebook.getLogoutUrl() + '">Logout</a>');
+    } else {
+      res.end('<a href="' + req.facebook.getLoginUrl() + '">Login</a>');
+    }
+    
+  })
+  .listen(port);
+
+console.log('Listening for http requests on port ' + port);
